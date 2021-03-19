c26f53b660a896456120dcf4142abad317dbef56
Ryan Morgan <nerdmomsrock@gmail.com>
2021-03-18 23:38:54 -0400

    just started social-mountain

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
-  }
-  
-  componentDidMount() {
-
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
 
-  updatePost() {
-  
-  }
+  componentDidMount() {}
 
-  deletePost() {
+  updatePost() {}
 
-  }
+  deletePost() {}
 
-  createPost() {
-
-  }
+  createPost() {}
 
   render() {
     const { posts } = this.state;
@@ -42,9 +35,7 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
           <Compose />
-          
         </section>
       </div>
     );
